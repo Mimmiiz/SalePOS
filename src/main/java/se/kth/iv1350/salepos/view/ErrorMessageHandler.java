921b1278331db82d83f36c46e17d9deff1b20dec
Maria Halvarsson <mahalv@kth.se>
2020-05-11 20:01:05 +0200

    Changes to error message print out.

---
+++ src/main/java/se/kth/iv1350/salepos/view/ErrorMessageHandler.java
@@ -0,0 +1,36 @@
+package se.kth.iv1350.salepos.view;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+/**
+ * This class handles error messages to the user.
+ */
+public class ErrorMessageHandler {
+    
+    /**
+     * Prints out error message to the user interface.
+     * The message includes date and time of the error and the error message.
+     * 
+     * @param message The error message that will be printed out.
+     */
+    void showErrorMessage (String message) {
+        StringBuilder errorMessageBuilder = new StringBuilder();
+        errorMessageBuilder.append("---------------- CASHIER NOTIFICATION ----------------\n");
+        errorMessageBuilder.append(getLocalDateAndTime());
+        errorMessageBuilder.append(", ERROR: \n");
+        errorMessageBuilder.append(message).append("\n");
+        errorMessageBuilder.append("-------------------------------------------------------\n");
+        System.out.println(errorMessageBuilder);
+    }
+    
+    /** 
+     * @return The local time and date as a String.
+     */
+    private String getLocalDateAndTime() {
+        LocalDateTime timeNow = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+        return timeNow.format(formatter);
+    }  
+}
