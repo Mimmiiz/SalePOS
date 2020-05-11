5185631e532ef7b22f094e809993a46c6bdd4019
Maria Halvarsson <mahalv@kth.se>
2020-05-12 00:02:18 +0200

    Created a log handler.

--- src/main/java/se/kth/iv1350/salepos/util/LogHandler.java
+++ src/main/java/se/kth/iv1350/salepos/util/LogHandler.java
@@ -1,6 +1,11 @@
-package util;
+package se.kth.iv1350.salepos.util;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 /**
  * Takes care of the exceptions and logs them to a text file.
@@ -8,4 +13,37 @@ import java.io.PrintWriter;
 public class LogHandler {
     private static final String LOG_FILE_NAME = "sale-dto-log.txt";
     private PrintWriter printWriter;
+    
+    /**
+     * Creates a new instance of the log handler.
+     * 
+     * @throws IOException 
+     */
+    public LogHandler() throws IOException {
+        try {
+            printWriter = new PrintWriter (new FileWriter(LOG_FILE_NAME), true);
+        } catch (IOException exc) {
+            System.out.println("Could not create LogHandler.");
+        }
+    }
+    
+    /**
+     * Logs the exception in a text file which includes the information about the thrown exception.
+     * 
+     * @param exc The exception that will be logged.
+     */
+    public void logException (Exception exc) {
+        StringBuilder logBuilder = new StringBuilder();
+        logBuilder.append(getDateAndTime());
+        logBuilder.append(", Exception was thrown: ");
+        logBuilder.append(exc.getMessage());
+        printWriter.println(logBuilder);
+        exc.printStackTrace(printWriter);
+    }
+    
+    private String getDateAndTime () {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+        return now.format(formatter);
+    }
 }
