a348b7bcb522181df33c71bf7c77fa1f965aa03c
Maria Halvarsson <mahalv@kth.se>
2020-05-11 13:52:31 +0200

    Name change that fits the method's purpose better.

--- src/test/java/se/kth/iv1350/salepos/integration/ItemRegistryTest.java
+++ src/test/java/se/kth/iv1350/salepos/integration/ItemRegistryTest.java
@@ -25,14 +25,14 @@ public class ItemRegistryTest {
     @Test
     public void testFindItemReturnCorrectValue() {
         ItemID itemIDinstance = new ItemID(89991);
-        ItemDTO itemDTOinstance = instanceToTest.findItem(itemIDinstance);
+        ItemDTO itemDTOinstance = instanceToTest.searchForItem(itemIDinstance);
         assertTrue(itemDTOinstance instanceof ItemDTO, "The ItemDTO was not created correcly.");
     }
     
     @Test
     public void testFindItemCorrectItemDTO() {
         ItemID itemIDinstance = new ItemID(60606);
-        ItemDTO itemDTOinstance = instanceToTest.findItem(itemIDinstance);
+        ItemDTO itemDTOinstance = instanceToTest.searchForItem(itemIDinstance);
         double expectedPrice = 8;
         double marginOfError = 0.01;
         assertEquals(expectedPrice, itemDTOinstance.getPrice().getAmount(), marginOfError, "The price did not get the expected value.");  
