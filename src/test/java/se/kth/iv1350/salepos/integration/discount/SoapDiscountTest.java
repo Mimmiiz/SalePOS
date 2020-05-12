a91a662938b0824ffa0a02cb1aa1ed61557c3227
Maria Halvarsson <mahalv@kth.se>
2020-05-12 23:46:08 +0200

    Initial commit.

---
+++ src/test/java/se/kth/iv1350/salepos/integration/discount/SoapDiscountTest.java
@@ -0,0 +1,18 @@
+package se.kth.iv1350.salepos.integration.discount;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import se.kth.iv1350.salepos.model.Amount;
+
+public class SoapDiscountTest {
+    SoapDiscount instanceToTest = new SoapDiscount();
+
+    @Test
+    public void testCalculateDiscount() {
+        Amount totalPrice = new Amount(100);
+        double expectedResult = 68.75;
+        Amount result = instanceToTest.calculateDiscount(totalPrice);
+        double marginOfError = 0.01;
+        assertEquals(expectedResult, result.getAmount(), marginOfError, "The price with discount was not corect.");
+    }   
+}
