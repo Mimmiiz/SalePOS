72718502307c3bd7388f1ab184a58888862d529d
Maria Halvarsson <mahalv@kth.se>
2020-05-12 12:05:17 +0200

    Added tests with exceptions.

--- src/test/java/se/kth/iv1350/salepos/model/SaleTest.java
+++ src/test/java/se/kth/iv1350/salepos/model/SaleTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import se.kth.iv1350.salepos.integration.ItemDTO;
+import org.junit.jupiter.api.Disabled;
 import se.kth.iv1350.salepos.integration.ItemRegistry;
+import se.kth.iv1350.salepos.integration.ItemRegistryException;
+import se.kth.iv1350.salepos.integration.NoSuchItemIdentifierException;
 import se.kth.iv1350.salepos.integration.RegistryCreator;
 
 public class SaleTest {
@@ -35,9 +37,48 @@ public class SaleTest {
     }
 
     @Test
-    public void testSearchForItemReturnValue() {
-        CurrentSaleDTO saleInfoInstance = instanceToTest.registerItem(itemID, itemReg);
-        assertTrue(saleInfoInstance instanceof CurrentSaleDTO, "System did not return the correct value.");
+    public void testRegisterItemReturnValue() {
+        try {
+            CurrentSaleDTO saleInfoInstance = instanceToTest.registerItem(itemID, itemReg);
+            assertTrue(saleInfoInstance instanceof CurrentSaleDTO, "System did not return the correct value.");
+        } catch (NoSuchItemIdentifierException | ItemRegistryException e) {
+            fail("Got exception.");
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void testRegisterItemThatDoesNotExistInRegistry() {
+        int identifierNumber = 10000;
+        ItemID itemID = new ItemID(identifierNumber);
+        try {
+            instanceToTest.registerItem(itemID, itemReg);
+            fail("Item was registered even if it does not exist in item registry.");
+        } catch (NoSuchItemIdentifierException e) {
+            assertTrue(e.getMessage().contains("" + identifierNumber), "Wrong exception message,"
+                    + " does not contain the specified item identifier: " + e.getMessage());
+            assertTrue(e.getItemIDThatDoesNotExist().checkItemIDMatch(itemID, itemID), " Wrong item identifier"
+                    + "is specified: " + e.getItemIDThatDoesNotExist());
+        } catch (ItemRegistryException e) {
+            fail("Got exception");
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public void testRegisterItemThatGivesRegistryError() {
+        int identifierNumber = 88888;
+        ItemID itemID = new ItemID(identifierNumber);
+        try {
+            instanceToTest.registerItem(itemID, itemReg);
+            fail("Item was registered.");
+        } catch (NoSuchItemIdentifierException e) {
+            fail("Got exception");
+            e.printStackTrace();
+        } catch (ItemRegistryException e) {
+            assertTrue(e.getMessage().contains("connect to the database"), "Exception did not contain the correct "
+                    + "error message: " + e.getMessage());
+        }
     }
     
     @Test
@@ -45,5 +86,23 @@ public class SaleTest {
         CurrentSaleDTO saleInfoInstance = instanceToTest.getSaleInfo();
         assertTrue(saleInfoInstance instanceof CurrentSaleDTO, "System did not return correct value");
     }
+    
+    @Test
+    public void testPayCorrectAmountOfChange() {
+        Amount amountToPay = new Amount(10);
+        CashPayment cashPayment = new CashPayment(amountToPay);
+        Amount changeInstance = instanceToTest.pay(cashPayment);
+        double expectedResult = 10;
+        double marginOfError = 0.01;
+        assertEquals(changeInstance.getAmount(), expectedResult, marginOfError, "Did not receive the correct "
+                + "amount of change.");
+    }
+    
+    @Disabled
+    public void testGetCompletedSaleInfo() {
+    }
+    
+    @Disabled
+    public void testPrintReceipt() {}
 }
 
