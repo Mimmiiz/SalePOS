78bf0b5ed495d9d4e136ad9dc4d8a48bef710971
Maria Halvarsson <mahalv@kth.se>
2020-05-12 15:40:30 +0200

    Initial commit.

---
+++ src/main/java/se/kth/iv1350/salepos/view/TotalRevenueView.java
@@ -0,0 +1,28 @@
+package se.kth.iv1350.salepos.view;
+
+import se.kth.iv1350.salepos.model.Amount;
+import se.kth.iv1350.salepos.model.SaleObserver;
+
+/**
+ * Prints the total revenue to the display.
+ */
+public class TotalRevenueView implements SaleObserver{
+    Amount totalRevenue = new Amount();
+    
+    /**
+     * Updates the total revenue.
+     * 
+     * @param totalPrice the total price that will be added to the total revenue.
+     */
+    @Override
+    public void updateTotalRevenue(Amount totalPrice) {
+        totalRevenue = (totalRevenue.add(totalPrice));
+        printOutTotalRevenue();
+    }
+    
+    private void printOutTotalRevenue() {
+        System.out.println("--------------- DISPLAY ---------------");
+        System.out.println("Total revenue: " + totalRevenue.getAmount());
+        System.out.println("-----------------------------------------\n");
+    }
+}
