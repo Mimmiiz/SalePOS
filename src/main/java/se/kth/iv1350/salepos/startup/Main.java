680018abcaeb046a7479d5e5ce16146047546e32
Maria Halvarsson <mahalv@kth.se>
2020-05-12 13:57:41 +0200

    Added IOException.

--- src/main/java/se/kth/iv1350/salepos/startup/Main.java
+++ src/main/java/se/kth/iv1350/salepos/startup/Main.java
@@ -1,5 +1,6 @@
 package se.kth.iv1350.salepos.startup;
 
+import java.io.IOException;
 import se.kth.iv1350.salepos.controller.Controller;
 import se.kth.iv1350.salepos.integration.ExternalCreator;
 import se.kth.iv1350.salepos.integration.Printer;
@@ -18,13 +19,17 @@ public class Main {
      * @param args The application does not take any command line parameters.
      */
     public static void main (String[] args){
-        RegistryCreator registryCreator = new RegistryCreator();
-        ExternalCreator externalCreator = new ExternalCreator();
-        Printer printer = new Printer();
-        Controller contr = new Controller(registryCreator, externalCreator, printer);
-        View view = new View(contr);
-        
-        view.runFakeExecution();
+        try {
+            RegistryCreator registryCreator = new RegistryCreator();
+            ExternalCreator externalCreator = new ExternalCreator();
+            Printer printer = new Printer();
+            Controller contr = new Controller(registryCreator, externalCreator, printer);
+            View view = new View(contr);
+            view.runFakeExecution();
+        } catch (IOException exc) {
+            System.out.println("The application could not start.");
+            exc.printStackTrace();
+            }
     }
     
 }
