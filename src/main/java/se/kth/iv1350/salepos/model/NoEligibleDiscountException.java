3573858887fd595d1583d322245be681a3882bf9
Maria Halvarsson <mahalv@kth.se>
2020-05-13 16:41:52 +0200

    Changed the discount operation.

---
+++ src/main/java/se/kth/iv1350/salepos/model/NoEligibleDiscountException.java
@@ -0,0 +1,20 @@
+package se.kth.iv1350.salepos.model;
+
+import se.kth.iv1350.salepos.model.CustomerID;
+
+/**
+ * Thrown when the <code>DiscountRegistry</code> can not find an eligible discount for the customer.
+ */
+public class NoEligibleDiscountException extends Exception {
+    private CustomerID customerID;
+    
+    /** 
+     * Creates a new instance that specifies that the customer with the specified customer ID is not eligible
+     * for any discounts.
+     * 
+     * @param customerID The ID of the customer.
+     */
+    public NoEligibleDiscountException() {
+        super("Customer is not eligible for any discounts.");
+    }
+}
