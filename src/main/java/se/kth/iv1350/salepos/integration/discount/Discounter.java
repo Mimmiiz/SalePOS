97f1801ce3a7934ea21027236e0a46c846bb843d
Maria Halvarsson <mahalv@kth.se>
2020-05-13 11:18:05 +0200

    Added comments.

--- src/main/java/se/kth/iv1350/salepos/integration/discount/Discounter.java
+++ src/main/java/se/kth/iv1350/salepos/integration/discount/Discounter.java
@@ -3,10 +3,16 @@ package se.kth.iv1350.salepos.integration.discount;
 import se.kth.iv1350.salepos.model.Amount;
 
 /**
- *
- * @author Maria
+ * An interface that defines the ability to calculate discounts for the sale. 
+ * The interface shall be implemented by classes that calculates discounts.
  */
 public interface Discounter {
     
+    /**
+     * Calculates the discount of a sale.
+     * 
+     * @param totalPrice The total price of the sale (without any discounts).
+     * @return The total price of the sale with the added discounts.
+     */
     Amount calculateDiscount(Amount totalPrice);
 }
