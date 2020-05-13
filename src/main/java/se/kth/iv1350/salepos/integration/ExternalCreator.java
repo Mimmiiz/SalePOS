af13940301eee26b3d763fd3939e94970daf7867
Maria Halvarsson <mahalv@kth.se>
2020-05-13 23:10:08 +0200

    Changed field to private.

--- src/main/java/se/kth/iv1350/salepos/integration/ExternalCreator.java
+++ src/main/java/se/kth/iv1350/salepos/integration/ExternalCreator.java
@@ -4,8 +4,8 @@ package se.kth.iv1350.salepos.integration;
  * This class is responsible for instantiating external systems.
  */
 public class ExternalCreator {
-    ExternalInventory externalInventory = new ExternalInventory ();
-    ExternalAccount externalAccount = new ExternalAccount ();
+    private ExternalInventory externalInventory = new ExternalInventory ();
+    private ExternalAccount externalAccount = new ExternalAccount ();
     
     /** 
      * Get the value of externalAccount.
