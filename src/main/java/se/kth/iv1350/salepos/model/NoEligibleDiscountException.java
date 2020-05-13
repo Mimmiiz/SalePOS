8ce1163f3f5f6399c7fd770225233b42ca1cdbe3
Maria Halvarsson <mahalv@kth.se>
2020-05-13 19:14:25 +0200

    Removed field.

--- src/main/java/se/kth/iv1350/salepos/model/NoEligibleDiscountException.java
+++ src/main/java/se/kth/iv1350/salepos/model/NoEligibleDiscountException.java
@@ -1,12 +1,9 @@
 package se.kth.iv1350.salepos.model;
 
-import se.kth.iv1350.salepos.model.CustomerID;
-
 /**
  * Thrown when the <code>DiscountRegistry</code> can not find an eligible discount for the customer.
  */
 public class NoEligibleDiscountException extends Exception {
-    private CustomerID customerID;
     
     /** 
      * Creates a new instance that specifies that the customer with the specified customer ID is not eligible
